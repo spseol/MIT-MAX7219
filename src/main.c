8738acb102c08b3f04c4f47ba11294c83387d5db
Marrek Nožka <marek@tlapicka.net>
2023-01-27 11:55:06 +0100

    ve škole...

--- src/main.c
+++ src/main.c
@@ -12,6 +12,8 @@
 #define HIGH(BAGR) GPIO_WriteHigh(BAGR##_PORT, BAGR##_PIN)
 #define REVERSE(BAGR) GPIO_WriteReverse(BAGR##_PORT, BAGR##_PIN)
 
+#include "max.h"
+
 void setup(void)
 {
     CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV1);      // taktovani MCU na 16MHz
@@ -23,19 +25,72 @@ void setup(void)
     init_milis();
 }
 
+void send_max(uint8_t address, uint8_t data) {
+    uint8_t mask;
+
+    LOW(CS);   // zahájení komunikace
+    
+    // posílám adresu
+    mask = 1 << 7;
+    mask = 0b10000000;
+    while (mask) {
+        if (address & mask) {   // nachystám DIN
+            HIGH(DIN);
+        } else {
+            LOW(DIN);
+        }
+        HIGH(CLK);    // vytvořím hodinový puls
+        mask = mask >> 1;
+        LOW(CLK);
+    }
+
+    // posílám data
+    mask = 1 << 7;
+    while (mask) {
+        if (data & mask) {   // nachystám DIN
+            HIGH(DIN);
+        } else {
+            LOW(DIN);
+        }
+        HIGH(CLK);    // vytvořím hodinový puls
+        mask = mask >> 1;
+        LOW(CLK);
+    }
+
+    HIGH(CS);  // ukončení komunikace
+}
+
 
 int main(void)
 {
     int32_t time = 0;
+    uint8_t bagr = 1;
 
     setup();
-
     LOW(CS);
+
+    send_max(DECODE_MODE, 0b01111111); // zapnout znakovou sadu na všech cifrách
+    send_max(SCAN_LIMIT, 7); // velikost displeje 8 cifer (počítáno od nuly, proto je argument číslo 7)
+    send_max(INTENSITY, 3); // volíme ze začátku nízký jas (vysoký jas může mít velkou spotřebu - až 0.25A !)
+    send_max(DISPLAY_TEST, 0); //
+    send_max(SHUTDOWN, 1);           // zapneme displej
+
+    send_max(7, 0);
+    send_max(6, 0);
+    send_max(5, 0);
+    send_max(4, 0);
+    send_max(3, 6);
+    send_max(2, 9);
+    send_max(1, 7);
+
     while (1) {
         if (milis() - time > 333) {
             time = milis();
-            REVERSE(DIN);
-
+            send_max(8, bagr);
+            bagr <<= 1;
+            if (bagr == 0) {
+                bagr = 1;
+            }
         }
     }
 }
